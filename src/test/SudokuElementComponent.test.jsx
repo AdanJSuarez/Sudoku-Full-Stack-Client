af109d91ecdc9e8f008d35a03d1064d7133ab285
Adan J. Suarez <adanjsuarez@gmail.com>
2018-10-30 08:13:16 -0400

    Included snapshots test for every component.

--- src/test/SudokuElementComponent.test.jsx
+++ src/test/SudokuElementComponent.test.jsx
@@ -1,40 +1,34 @@
 import React from 'react';
 import Enzyme, {shallow, render, mount} from 'enzyme';
 // import sinon from 'sinon';
-import App from '../App';
 import Adapter from 'enzyme-adapter-react-16';
 import {createSerializer} from 'enzyme-to-json';
 //import { expect } from 'chai';
-// import SudokuComponent from '../components/Sudoku';
+import SudokuElementComponent from '../components/SudokuElement';
 
 //Set the default serializer for jest to be from enzyme-to-json
 //this produce a more easy to read serialized format
 expect.addSnapshotSerializer(createSerializer({mode: "deep"}));
 Enzyme.configure({adapter: new Adapter()});
 
-describe("Test suit for App", ()=>{
-    const sudokuSolved = [2,5,8,7,4,3,9,6,1,6,7,1,5,9,8,2,3,4,3,4,9,2,1,6,8,7,5,5,6,
-        3,9,7,1,4,8,2,1,8,2,3,5,4,7,9,6,7,9,4,8,6,2,1,5,3,4,1,7,6,8,5,3,2,9,9,3,6,
-        1,2,7,5,4,8,8,2,5,4,3,9,6,1,7];
+describe("Test suit for SudokuElementComponent", ()=>{
     
     it('render correctly shallow', ()=>{
         const wrapper = shallow(
-            <App sudokuNumbers={sudokuSolved} />
+            <SudokuElementComponent row={1} row={1} column={1} dataElement={2} toggledNumber={{number:2, row:1, column:1}} />
         );
         expect(wrapper).toMatchSnapshot();
     });
     it('render correctly render', ()=>{
         const wrapper = render(
-            <App sudokuNumbers={sudokuSolved} />
+            <SudokuElementComponent row={1} row={1} column={1} dataElement={2} toggledNumber={{number:2, row:1, column:1}} />
         );
         expect(wrapper).toMatchSnapshot();
     });
     it('render correctly mount', () => {
-        const wrapper = mount(<App sudokuNumbers={sudokuSolved}/>);
+        const wrapper = mount(<SudokuElementComponent row={1} row={1} column={1} dataElement={2} toggledNumber={{number:2, row:1, column:1}}/>);
         expect(wrapper).toMatchSnapshot();
     });
-    it('render after update state has to include sudoku numbers', ()=>{
-        // TODO: Previos tries didn't work. I need to see why
-    });
+    
     //TODO: It needs many more test!!
 })
--- src/test/SudokuLineComponent.test.jsx
+++ src/test/SudokuLineComponent.test.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
 import Enzyme, {shallow, render, mount} from 'enzyme';
 // import sinon from 'sinon';
-import App from '../App';
 import Adapter from 'enzyme-adapter-react-16';
 import {createSerializer} from 'enzyme-to-json';
 //import { expect } from 'chai';
-// import SudokuComponent from '../components/Sudoku';
+import SudokuLineComponent from '../components/SudokuLine';
 
 //Set the default serializer for jest to be from enzyme-to-json
 //this produce a more easy to read serialized format
 expect.addSnapshotSerializer(createSerializer({mode: "deep"}));
 Enzyme.configure({adapter: new Adapter()});
 
-describe("Test suit for App", ()=>{
-    const sudokuSolved = [2,5,8,7,4,3,9,6,1,6,7,1,5,9,8,2,3,4,3,4,9,2,1,6,8,7,5,5,6,
-        3,9,7,1,4,8,2,1,8,2,3,5,4,7,9,6,7,9,4,8,6,2,1,5,3,4,1,7,6,8,5,3,2,9,9,3,6,
-        1,2,7,5,4,8,8,2,5,4,3,9,6,1,7];
+describe("Test suit for SudokuLineComponent", ()=>{
+    const dataLine = [2,5,8,7,4,3,9,6,1];
     
     it('render correctly shallow', ()=>{
         const wrapper = shallow(
-            <App sudokuNumbers={sudokuSolved} />
+            <SudokuLineComponent row={1} dataLine={dataLine} toggledNumber={{number:2, row:1, column:1}} />
         );
         expect(wrapper).toMatchSnapshot();
     });
     it('render correctly render', ()=>{
         const wrapper = render(
-            <App sudokuNumbers={sudokuSolved} />
+            <SudokuLineComponent row={1} dataLine={dataLine} toggledNumber={{number:2, row:1, column:1}} />
         );
         expect(wrapper).toMatchSnapshot();
     });
     it('render correctly mount', () => {
-        const wrapper = mount(<App sudokuNumbers={sudokuSolved}/>);
+        const wrapper = mount(<SudokuLineComponent row={1} dataLine={dataLine} toggledNumber={{number:2, row:1, column:1}}/>);
         expect(wrapper).toMatchSnapshot();
     });
-    it('render after update state has to include sudoku numbers', ()=>{
-        // TODO: Previos tries didn't work. I need to see why
-    });
+    
     //TODO: It needs many more test!!
 })
